c35a3ff00facf2b84a831dfea1e6a888b8092253
StanB <stanb791@gmail.com>
2024-10-10 14:24:40 +0300

    Initial commit

---
+++ app/src/main/java/com/example/lab1/MainActivity2.java
@@ -0,0 +1,36 @@
+package com.example.lab1;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @SuppressLint("SetTextI18n")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main2);
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        setContentView(R.layout.activity_main2);
+        TextView text = findViewById(R.id.textView2);
+        Bundle extras = getIntent().getExtras();
+        assert extras != null;
+        text.setText("Переданный параметр: " + extras.getString("surname"));
+
+    }
+}
+
